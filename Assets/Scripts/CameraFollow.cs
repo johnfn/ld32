dcd11ef36530e5058c2be57771e5b060b545ef57
Grant Mathews <johnfn@gmail.com>
2015-04-17 21:36:40 -0700

    Fix camera to map bounds.
    Code worked on the first try. Awesome

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,20 +7,38 @@ public class CameraFollow : MonoBehaviour
 
     public float Height
     {
-        get { return 2.0f * GetComponent<Camera>().pixelHeight; }
+        get { return 2.0f * _camera.orthographicSize; }
     }
 
     public float Width
     {
-        get { return 2.0f * GetComponent<Camera>().pixelWidth; }
+        get { return Height * _camera.aspect; }
+    }
+
+    private Camera _camera;
+
+    void Awake()
+    {
+        _camera = GetComponent<Camera>();
     }
 
 	void LateUpdate()
 	{
-	    transform.position = new Vector3 (
-            Target.transform.position.x,
-            Target.transform.position.y,
-            transform.position.z
-        );
+	    var minX = Mathf.Floor(Target.transform.position.x / Util.MapWidth) * Util.MapWidth;
+	    var minY = Mathf.Floor(Target.transform.position.y / Util.MapHeight) * Util.MapHeight;
+
+	    var maxX = minX + Util.MapWidth;
+	    var maxY = minY + Util.MapHeight;
+
+	    minX += Width / 2; 
+	    maxX -= Width / 2; 
+
+	    minY += Height / 2; 
+	    maxY -= Height / 2;
+
+	    var camX = Mathf.Clamp(Target.transform.position.x, minX, maxX);
+	    var camY = Mathf.Clamp(Target.transform.position.y, minY, maxY);
+
+	    transform.position = new Vector3(camX, camY, transform.position.z);
 	}
 }
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Util
+{
+    public static float MapWidth = 8.0f;
+    public static float MapHeight = 8.0f;
+}
