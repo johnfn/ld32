a89a914e450c28b0ca2508a8bc457db19fb0bc8d
Grant Mathews <johnfn@gmail.com>
2015-04-19 14:45:02 -0700

    Ugh that took too long
    uv got to be kidding me. get it?

---
+++ Assets/Scripts/OffsetScroller.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using JetBrains.Annotations;
+
+public class OffsetScroller : MonoBehaviour
+{
+    private Vector3 _initialPosition;
+
+    private GameObject _secondGuy;
+
+    private MeshRenderer _renderer;
+
+    public Vector3 Speed = new Vector3(-.1f, 0f, 0f);
+
+    public bool IsClone = false;
+
+    void Awake()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        _initialPosition = transform.position;
+    }
+
+    [UsedImplicitly]
+    void Start()
+    {
+        if (IsClone) return;
+
+        _secondGuy = Instantiate(gameObject);
+        _secondGuy.GetComponent<OffsetScroller>().IsClone = true;
+        _secondGuy.transform.position = new Vector3(_initialPosition.x + _renderer.bounds.size.x, _initialPosition.y,
+            _initialPosition.z);
+    }
+
+    [UsedImplicitly]
+    void Update()
+    {
+        transform.position += Speed * Time.deltaTime;
+
+        if (transform.position.x + _renderer.bounds.size.x < _initialPosition.x)
+        {
+            transform.position += new Vector3(2 * _renderer.bounds.size.x, 0f, 0f);
+        }
+    }
+}
+
