2279cf869cc42f4625cd7aed05f53a34f472d5eb
Grant Mathews <johnfn@gmail.com>
2015-04-19 15:10:03 -0700

    Moar parallax

---
+++ Assets/Scripts/Parallaxer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxer : MonoBehaviour
+{
+    public GameObject ParallaxTarget;
+
+    public Vector3 ParallaxFactor;
+
+    private Vector3 _initialTargetPosition;
+
+    private Vector3 _initialParallaxScrollerPosition;
+
+    public void Start()
+    {
+        _initialTargetPosition = ParallaxTarget.transform.position;
+        _initialParallaxScrollerPosition = gameObject.transform.position;
+    }
+
+    public void LateUpdate()
+    {
+        var targetDiff = ParallaxTarget.transform.position - _initialTargetPosition;
+
+        gameObject.transform.position = _initialParallaxScrollerPosition + Vector3.Scale(targetDiff, ParallaxFactor);
+    }
+}
